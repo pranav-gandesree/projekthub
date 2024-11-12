61c869dd25bafac3fa8a5c37fd7de55c9f30e888
pranav <pranav8267@gmail.com>
2024-11-12 14:04:25 +0530

    added loader to the signin button

--- components/form/SignInForm.tsx
+++ components/form/SignInForm.tsx
@@ -1,131 +1,3 @@
-// "use client"
-
-// import * as z from "zod";
-// import { useForm } from "react-hook-form";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import { Button } from "@/components/ui/button";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import Link from "next/link";
-// import GoogleSignInButton from "../GoogleSignInButton";
-// import { signIn } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { useToast } from "@/components/ui/use-toast"
-
-
-// const FormSchema = z.object({
-//   email: z.string().min(1, "Email is required").email("Invalid email"),
-//   password: z.string()
-//     .min(1, "Password is required")
-//     .min(8, "Password must have at least 8 characters"),
-// });
-
-// const SignInForm = () => {
-//   const router = useRouter();
-//   const { toast } = useToast()
-
-//   const form = useForm<z.infer<typeof FormSchema>>({
-//     resolver: zodResolver(FormSchema),
-//     defaultValues: {
-//       email: "",
-//       password: "",
-//     },
-//   });
-
-//   const onSubmit = async (values: z.infer<typeof FormSchema>) => {
-//     const signInData = await signIn("credentials", {
-//       redirect: false,
-//       email: values.email,
-//       password: values.password,
-//     });
-
-//     if (signInData?.error) {
-//       console.log("Sign-in Error:", signInData.error);
-//      toast({
-//           title: "Error!",
-//           description: "Oops!! Something is wrong",
-//           variant: "destructive"
-//         })
-//     } else {
-//       console.log("Sign-in successful, redirecting to admin...");
-//       router.refresh();
-//       router.push("/home");
-//     }
-//   };
-
-//   return (
-//     <>
-
-//     <div>
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-//           <div className="space-y-2">
-//             <FormField
-//               control={form.control}
-//               name="email"
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel className="text-slate-700">Email</FormLabel>
-//                   <FormControl>
-//                     <Input placeholder="Email" {...field}    className="text-slate-700" />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-//             <FormField
-//               control={form.control}
-//               name="password"
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel className="text-slate-700">Password</FormLabel>
-//                   <FormControl>
-//                     <Input
-//                       placeholder="Enter your password"
-//                       type="password"
-//                       {...field}
-//                       className="text-slate-700"
-//                     />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-//           </div>
-//           <Button className="w-full mt-6" type="submit">
-//             Sign In
-//           </Button>
-//         </form>
-
-//         <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400 text-slate-700">
-//           or
-//         </div>
-
-//         <GoogleSignInButton>Sign In with Google</GoogleSignInButton>
-
-//         <p className="mt-4 text-slate-700">
-//           If you don&apos;t have an account, please&nbsp;
-//           <Link className="text-blue-500 hover:underline" href="/signup">
-//             Sign Up
-//           </Link>
-//         </p>
-//       </Form>
-//     </div>
-//     </>
-//   );
-// };
-
-// export default SignInForm;
-
-
-
 'use client'
 
 import * as z from "zod";
@@ -145,7 +17,9 @@ import Link from "next/link";
 import GoogleSignInButton from "../GoogleSignInButton";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import {Loader} from "lucide-react"
 
 const FormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email"),
@@ -157,6 +31,7 @@ const FormSchema = z.object({
 const SignInForm = () => {
   const router = useRouter();
   const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -167,6 +42,7 @@ const SignInForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    setLoading(true);
     const signInData = await signIn("credentials", {
       redirect: false,
       email: values.email,
@@ -180,6 +56,7 @@ const SignInForm = () => {
         description: "Oops!! Something is wrong",
         variant: "destructive",
       });
+      setLoading(false);
     } else {
       console.log("Sign-in successful, redirecting to admin...");
       router.refresh();
@@ -236,6 +113,7 @@ const SignInForm = () => {
                 />
               </div>
               <Button className="w-full mt-6" type="submit">
+              {loading && <Loader className="animate-spin h-5 w-5 text-white" />}
                 Sign In
               </Button>
             </form>
@@ -244,7 +122,9 @@ const SignInForm = () => {
               or
             </div>
 
-            <GoogleSignInButton>Sign In with Google</GoogleSignInButton>
+            <GoogleSignInButton>
+            Sign In with Google
+            </GoogleSignInButton>
 
             <p className="mt-4 text-slate-700">
               If you don&apos;t have an account, please&nbsp;
