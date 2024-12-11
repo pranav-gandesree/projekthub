e134f093f6397100d87c8be9855251cb14fb821b
pranav <pranav8267@gmail.com>
2024-12-11 22:52:07 +0530

    fixed &apos; issue

--- components/canvas/PageNotFound.tsx
+++ components/canvas/PageNotFound.tsx
@@ -11,7 +11,7 @@ export default function PageNotFound() {
           Oops! Page not found
         </h2>
         <p className="text-lg text-gray-400 animate-fade-in">
-          The page you're looking for doesn't exist or has been moved.
+           The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
         <div className="flex justify-center space-x-4 animate-slide-up">
           <Link 
